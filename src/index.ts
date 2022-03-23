09382cda4819260f0619e96214d0a2efea289027
Peter Hughes <phughes@everquote.com>
2022-03-23 16:11:12 +0000

    add tslint comment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult
-} from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  console.log("Request: ", JSON.stringify(event, undefined, 2))
-  const queries = JSON.stringify(event.queryStringParameters);
-  return {
-    statusCode: 200,
-    body: `Queries: ${queries}`
-  };
+export const lambdaHandler = async (
+    event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+    // tslint:disable-next-line:no-console
+    console.log('Request: ', JSON.stringify(event, undefined, 2));
+    const queries = JSON.stringify(event.queryStringParameters);
+    return {
+        statusCode: 200,
+        body: `Queries: ${queries}`,
+    };
 };
