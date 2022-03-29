d69b38913752dd2d6f31c82fa38ffa06a83bd91f
Peter Hughes <phughes@everquote.com>
2022-03-29 15:41:21 +0100

    add bucket

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -1,20 +1,36 @@
-import { App, Duration, Stack, StackProps } from "aws-cdk-lib";
+import { App, aws_s3 as s3, Duration, Stack, StackProps } from "aws-cdk-lib";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { Effect, PolicyStatement, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 
 export class TsLambdaStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new NodejsFunction(this, 'helloWorldFn', {
+    new NodejsFunction(this, "helloWorldFn", {
       runtime: Runtime.NODEJS_14_X,
-      entry: 'src/index.ts',
-      handler: 'lambdaHandler',
+      entry: "src/index.ts",
+      handler: "lambdaHandler",
       timeout: Duration.seconds(30),
       memorySize: 128,
       bundling: {
-        minify: true,
+        minify: true
       }
-    })
+    });
+
+    const bucket = new s3.Bucket(this, "CdkTestBucket", {
+      encryption: s3.BucketEncryption.KMS,
+      bucketKeyEnabled: true,
+      enforceSSL: true
+    });
+
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        principals: [new ServicePrincipal("lambda.amazonaws.com")],
+        actions: ["s3.GetObject"],
+        resources: [`${bucket.bucketArn}`]
+      })
+    );
   }
 }
