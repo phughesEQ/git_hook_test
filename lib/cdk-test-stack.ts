3b1799b1b56650d0a308b17d5757d67c91f62861
Peter Hughes <phughes@everquote.com>
2022-07-27 16:04:11 +0100

    add dynamodb

--- lib/cdk-test-stack.ts
+++ lib/cdk-test-stack.ts
@@ -1,26 +1,43 @@
-import { App, aws_s3 as s3, Duration, Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Duration, Stack, StackProps } from "aws-cdk-lib";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { FunctionUrlAuthType, Runtime} from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
 
 export class TsLambdaStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new NodejsFunction(this, "helloWorldFn", {
-      runtime: Runtime.NODEJS_14_X,
+     const table = new Table(this, "Hello", {
+      partitionKey: { name: "name", type: AttributeType.STRING },
+    });
+
+    const dynamoLambda = new NodejsFunction(this, "helloWorldFn", {
+      runtime: Runtime.NODEJS_16_X,
       entry: "src/index.ts",
-      handler: "lambdaHandler",
+      handler: "handler",
       timeout: Duration.seconds(30),
       memorySize: 128,
       bundling: {
         minify: true
+      },
+      environment: {
+        HELLO_TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantReadWriteData(dynamoLambda);
+
+    const myFunctionUrl = dynamoLambda.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ['*'],
       }
     });
 
-    new s3.Bucket(this, "CdkTestBucket", {
-      encryption: s3.BucketEncryption.KMS,
-      bucketKeyEnabled: true,
-      enforceSSL: true
+    new CfnOutput(this, 'FunctionUrl', {
+      value: myFunctionUrl.url,
     });
+
   }
 }
